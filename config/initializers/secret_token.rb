4b6fcb1f37020a609c8f363824fa343dd7a481ab
Stephane Perrin <stephaneper@gmail.com>
2013-03-09 20:27:47 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Budeal::Application.config.secret_token = '7eee764d1f5f971736f567bf64a3e0a6cc81a328c5fe4d2e5c3e4dc1a7de89bce4fa6d5d7f57282bb916e597e1a7d74641a60d666ba97793c9e7ca8e1090df7c'
