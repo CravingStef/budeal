08141f6fdbf84bcbe7afc10fd56afbe7d83ec317
Stephane Perrin <stephaneper@gmail.com>
2013-03-17 14:03:35 +0100

    Add Pins Associations

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,5 +1,8 @@
 class Pin < ActiveRecord::Base
-  attr_accessible :description
+  attr_accessible :description, :price
 
   validates :description, presence: true, :length => { :maximum => 100}
+  validates :user_id, presence: true
+  
+  belongs_to :user
 end
