b882db4476e357206131a00426cfaea89fc6ca71
Stephane Perrin <stephaneper@gmail.com>
2013-03-19 23:31:30 +0100

    Added Login in modal

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+#aide pour login via modal
+def resource_name
+  :user
+end
+
+def resource
+  @resource ||= User.new
+end
+
+def devise_mapping
+ @devise_mapping ||= Devise.mappings[:user]
+end
+#fin aide
 end
